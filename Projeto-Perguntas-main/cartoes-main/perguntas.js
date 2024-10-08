cdb0811019bbf5aa1065e9a297be29d05b552d5a
Madarinha-Fake <164112828+Madarinha-Fake@users.noreply.github.com>
2024-10-08 09:45:21 -0300

    New

---
+++ Projeto-Perguntas-main/cartoes-main/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Pergunta 1',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    '“Je pense, donc je suis” é a frase original, escrita em francês, do filósofo René Descartes (1596-1650). Ela resume o pensamento e o método de Descartes, para quem tudo tem início na dúvida.'
+)
+
+criaCartao(
+    'Pergunta 2',
+    'De onde é a invenção do chuveiro elétrico?',
+    'Francisco Canhos é o nome do homem que, na década de 40, desenvolveu o primeiro chuveiro elétrico seguro no Brasil.'
+)
+
+criaCartao(
+    'Pergunta 3',
+    'Qual o menor país do mundo?',
+    'O Vaticano, sede da igreja católica, tem apenas 44 hectares (0,44 km2).'
+)
+
+criaCartao(
+    'Pergunta 4',
+    'Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'João Belchior Marques Goulart (1919-1976) foi o 24º presidente do Brasil.'
+)
+
+criaCartao(
+    'Pergunta 5',
+    'Quais os países que têm a maior e a menor expectativa de vida do mundo?',
+    'No Japão, a expectativa de vida é em média 84 anos, enquanto na Serra Leoa é 53 anos.'
+)
+
+criaCartao(
+    'Pergunta 6',
+    'O que a palavra legend significa em português?',
+    'Lenda.'
+)
